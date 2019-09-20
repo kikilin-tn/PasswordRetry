e75fbf3164b92430a5978ad3b03e126890c33986
Kiki Lin <mikey2862@gmail.com>
2019-09-20 10:44:54 +0800

    input cnt from 3

--- pw_input.py
+++ pw_input.py
@@ -1,14 +1,14 @@
 password = 'a12345'
-input_cnt = 1
-print("input 'q' if you want to quit" )
-while input_cnt <= 3:
+input_cnt = 3
+
+while input_cnt > 0:
     user_input = input('please input 6 digits for password: ')
     if user_input == password:
         print('log in sucessfully')
-    elif user_input == 'q':
         break
     else:
-        print('please try again')
-
-    print('you already input '+ str(input_cnt)+' times')
-    input_cnt +=1
+        input_cnt = input_cnt - 1
+    if input_cnt > 0:
+        print('remain '+ str(input_cnt)+' times')
+    else:
+        print('input count is over 3 times')
